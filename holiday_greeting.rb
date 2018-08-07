19df790f0f88630a66130fb58913e5a540ea412f
christinasherpa <chrisher24@charter.newvisions.org>
2018-08-07 15:58:10 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,5 @@
+def holiday_greeting ( holiday, sender, receiver)
+  puts "#{holiday}! With much love from #{sender}, to #{receiver}."
+end
+
+holiday_greeting("Merry Christmas", "Christina", "Keren")
